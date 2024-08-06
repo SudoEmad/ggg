904be2edcf24172350b94fab6eac5cbd61caa9da
SudoEmad <mr.premuim1@gmail.com>
2024-08-06 20:11:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import http.server
+import socketserver
 
+PORT = 8080
+
+# Create a handler for the HTTP server
+Handler = http.server.SimpleHTTPRequestHandler
+
+# Create an HTTP server and bind it to the specified port
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving HTTP on port {PORT}")
+    httpd.serve_forever()
